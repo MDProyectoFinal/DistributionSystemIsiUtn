b4214d26de85581d99fd82d15ab0a02e72429ce0
Matias Clement <matiasclement04@gmail.com>
2023-11-09 17:21:50 -0300

    Cambio del mongoose.connect para apuntar a Atlas Mongo DB

--- api/index.js
+++ api/index.js
@@ -6,7 +6,12 @@ var port = process.env.PORT || 3977;
 
 mongoose.Promise = global.Promise;
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://0.0.0.0:27017/rosario_snack').then( 
+
+// Conectar local a mi Mongo DB
+// mongoose.connect('mongodb://0.0.0.0:27017/rosario_snack').then( 
+
+// Conectar Mongo DB Atlas Free Online
+mongoose.connect('mongodb+srv://proyectofinalisiutn:ProyectoFinalIsiUtn2023@clusterbd.q87qmbk.mongodb.net/rosario_snack').then( 
     res => {
         console.log(`La conexión a la base de datos está funcionando correctamente..`);  
         
